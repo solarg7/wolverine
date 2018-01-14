4b82f7f4811bf54b9dc9ff21bd9026a638b7673f
jbluft <jbluft@gmail.com>
2018-01-14 14:07:35 -0500

    fixing modal, css tweaks

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -169,7 +169,13 @@ window.onload = function() {
 						value: movie.name
 					}).appendTo('#created3');
 				}
-			});
+
+				else {
+					console.log("movie not found")
+				$('#tipModal').modal('show');
+  				}
+		});
+
 
 
 
@@ -273,66 +279,6 @@ window.onload = function() {
 			var aux = top3Movies [i];
 			console.log(aux);
 		}
-/*
-	//on click for the modal tip
-	(function(){
-  var $content = $('#modal-tip').detach();   // Remove modal from page
-
-  $('#tip').on('click', function() {           // Click handler to open modal
-    modal.open({content: $content, width:340, height:300});
-  });
-}());
-	//end on click for the modal tip
-
-
-	//modal window js
-	var modal = (function() {                         // Declare modal object
-
-  var $window = $(window);                        // Store the window
-  var $modal = $('<div class="modal"/>');         // Create markup for modal
-  var $content = $('<div class="modal-content"/>');
-  var $close = $('<button role="button" class="modal-close">close</button>');
-
-  $modal.append($content, $close);                // Add close button to modal
-
-  $close.on('click', function(e){                 // If user clicks on close
-    e.preventDefault();                           // Prevent link behavior
-    modal.close();                                // Close the modal window
-  });
-
-  return {                                        // Add code to modal
-    center: function() {                          // Define center() method
-      // Calculate distance from top and left of window to center the modal
-      var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
-      var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
-      $modal.css({                                // Set CSS for the modal
-        top:top + $window.scrollTop(),            // Center vertically
-        left:left + $window.scrollLeft()          // Center horizontally
-      });
-    },
-    open: function(settings) {                     // Define open() method
-      $content.empty().append(settings.content);   // Set new content of modal
-
-      $modal.css({                                 // Set modal dimensions
-        width: settings.width || 'auto',           // Set width
-        height: settings.height || 'auto'          // Set height
-      }).appendTo('body');                         // Add it to the page
-
-      modal.center();                              // Call center() method
-      $(window).on('resize', modal.center);        // Call it if window resized
-    },
-    close: function() {                            // Define close() method
-      $content.empty();                            // Remove content from modal
-      $modal.detach();                             // Remove modal from page
-      $(window).off('resize', modal.center);       // Remove event handler
-    }
-  };
-}());
-	//end modal js
-*/
-
-
-
 
 
 
