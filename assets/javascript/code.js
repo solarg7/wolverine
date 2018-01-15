5845745e45245136b043e4f05084e32428872685
jbluft <jbluft@gmail.com>
2018-01-15 13:36:24 -0500

    pull

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -126,35 +126,6 @@ window.onload = function() {
 	  	firebase.initializeApp(config);
 
 
-	  	//Google sign-in section
-	  	var provider = new firebase.auth.GoogleAuthProvider();
-	  	firebase.auth().signInWithRedirect(provider);
-
-	  	firebase.auth().getRedirectResult().then(function(result) {
-  			if (result.credential) {
-    			// This gives you a Google Access Token. You can use it to access the Google API.
-    			var token = result.credential.accessToken;
-    			// ...
-  				}
-  				// The signed-in user info.
-  				var user = result.user;
-				}).catch(function(error) {
-  				// Handle Errors here.
-  				var errorCode = error.code;
-  				var errorMessage = error.message;
-  				// The email of the user's account used.
-  				var email = error.email;
-  				// The firebase.auth.AuthCredential type that was used.
-  				var credential = error.credential;
-  				// ...
-				});
-
-	  	firebase.auth().signOut().then(function() {
- 			// Sign-out successful.
-			}).catch(function(error) {
-  			// An error happened.
-			});
-		//end google sign-in
 
 
 
